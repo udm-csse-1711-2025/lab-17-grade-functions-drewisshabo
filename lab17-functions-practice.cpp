5e7ab99c55996bde0a49b04e01df9aae97760c53
drewisshabo <andrewszabo394@gmail.com>
2025-12-10 22:26:38 -0500

    Add files via upload

--- lab17-functions-practice.cpp
+++ lab17-functions-practice.cpp
@@ -2,7 +2,7 @@
 // Gets three exam scores from the user and shows them 
 // their letter grades
 // Started by: Caitlin Snyder
-// Edited by: [NAME HERE]
+// Edited by: [Andrew Szabo]
 
 #include <iostream>
 using namespace std;
@@ -12,14 +12,20 @@ using namespace std;
  *  Purpose: Ask the user for a test score, validate the input,
  *          and return the valid score.
  *  Parameters: None
- *  Return Value: A double representing a valid test score (0–100)
+ *  Return Value: A double representing a valid test score (0ï¿½100)
  ****************************************************************/
 
-// [TO DO: ADD getScore function HERE]
-    // Ask the user for a score and validate input.
-    // Keep asking until the score is between 0 and 100.
-    // Return the valid score.
+double getScore(){ // function returns double with no parameters
+  double score;
+  cout << "Enter your test score 0-100" << endl;
+  cin >> score;
 
+  while(score < 0 || score > 100){ // gets a real input 
+    cout << "Invalid test score try again" << endl;
+    cin >> score;
+  }
+  return score;
+}
 
 
 
@@ -28,18 +34,27 @@ using namespace std;
  * Function Name: displayLetterGrade
  * Purpose: Determine and display the letter grade 
  *          corresponding to the numeric score.
- * Parameters: score (double) – the numeric test score
+ * Parameters: score (double) ï¿½ the numeric test score
  * Return Value: None (displays the letter grade to the screen)
  ****************************************************************/
 
-// [TO DO : ADD getScore function HERE]
-    // Determine the letter grade and display it.
-    // Example grading scale:
-    // 90–100: A
-    // 80–89:  B
-    // 70–79:  C
-    // 60–69:  D
-    // Below 60: F
+void displayLetterGrade(double score){ // function that returns nothing but takes in a double and calculates letter grade
+  if(score >= 90){ // if higher than 90 A
+    cout << 'A' << endl;
+  }
+  else if(score >= 80){ // higher than 80 B
+    cout << 'B' << endl;
+  }
+  else if(score >= 70){ //higher than 70 C
+    cout << 'C' << endl;
+  }
+  else if(score >= 60){ //higher than 60 D
+    cout << 'D' << endl;
+  }
+  else{ // if lower F
+    cout << 'F' << endl;
+  }
+}
 
 
 
